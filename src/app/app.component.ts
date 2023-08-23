4d8f1a7121a1f88e249745f93f249b8446a33dbd
arthur <a.lannelucq@gmail.com>
2023-08-23 20:07:47 +0200

    refactor: use inline template for app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,17 @@ import { IfAdminDirective } from "./directives/if-admin.directive";
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
+  template: `
+    <header>
+      <a routerLinkActive="active" routerLink="" [routerLinkActiveOptions]="{ exact: true }">Accueil</a>
+      <a routerLinkActive="active" routerLink="dashboard" *ifAdmin>Dashboard</a>
+      <a routerLinkActive="active" routerLink="orders" *ifAuthenticated>Mes commandes</a>
+      <a routerLinkActive="active" routerLink="profile" *ifAuthenticated>Mon Profil</a>
+    </header>
+    <div class="content">
+      <router-outlet/>
+    </div>
+  `,
   styleUrls: ['./app.component.scss'],
   imports: [RouterModule, IfAuthenticatedDirective, IfAdminDirective]
 })
